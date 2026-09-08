9fdc2cf2d1d82deb478994804eed82dd5842d0ae
josemlwdf <josemlyandex@gmail.com>
2026-09-08 09:38:23 +0200

    Fixed some manifers and Docker files issues

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from sekoia_formatter_modules.action_format import FormatAction
 
 if __name__ == "__main__":
     module = SekoiaFormatterModule()
-    module.register(FormatAction, "format")
+    module.register(FormatAction, "FormatAction")
     module.run()
