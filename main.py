cfc7da500055af7bbe215af948256b13d7671b23
josemlwdf <josemlyandex@gmail.com>
2026-09-08 10:23:31 +0200

    chore: bump version to 0.4.0

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from sekoia_formatter_modules import SekoiaFormatterModule
 
 from sekoia_formatter_modules.action_format import FormatAction
 
-
 if __name__ == "__main__":
     module = SekoiaFormatterModule()
     module.register(FormatAction, "FormatAction")
